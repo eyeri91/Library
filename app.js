6d44ffe70479ca46885d18ec15e6a677a8662cde
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-08-18 23:34:29 +0400

    Add createBookCard function for UI

--- app.js
+++ app.js
@@ -32,7 +32,52 @@ const Display = document.getElementsByClassName('books');
 
 function createBookCard(book) {
     const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
+    const cardBody = document.createElement('div');
+    const title = document.createElement('h3')
+    const author = document.createElement('h4');
+    const pages = document.createElement('h4');
+    const status = document.createElement('div');
+    const checkInput = document.createElement('input');
+    const checkLabel = document.createElement('label');
+    const btns = document.createElement('div');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    Display.append(bookCard);
+    bookCard.append(cardBody);
+    cardBody.append(title, author, pages, status, btns);
+    status.append(checkInput, checkLabel);
+    btns.append(editBtn, deleteBtn);
+
+    bookCard.classList.add('book-card', 'card');
+    cardBody.classList.add('card-body');
+    title.classList.add('book-title', 'card-title', 'mb-3');
+    author.classList.add('book-author', 'card-title', 'mb-3');
+    pages.classList.add('book-page', 'card-title', 'mb-2');
+    status.classList.add('book-status', 'form-check');
+    checkInput.classList.add('form-check-input');
+    checkInput.id = "flexCheckDefault";
+    checkInput.type = "checkbox";
+    checkLabel.classList.add('form-check-label');
+    checkLabel.htmlFor = "flexCheckDefault";
+
+    // buttons
+    btns.classList.add('book-btns');
+    editBtn.classList.add('btn', 'edit-btn');
+    editBtn.type = "button";
+    deleteBtn.classList.add('btn', 'delete-btn');
+    deleteBtn.type = "button";
+
+    // Book details.
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+    checkInput.checked = book.isRead;
+    checkLabel.innerText = "Finished";
+    editBtn.innerText = "Edit"
+    editBtn.innerText = "Delete";
+
+
 }
 
 
